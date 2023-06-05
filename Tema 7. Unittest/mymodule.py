066126c3215ee9018819a43f97a3107fd5475420
Carlos Oliva <colidom@outlook.com>
2023-06-05 18:09:12 +0100

    Añadidos métodos y sus tests

--- Tema 7. Unittest/mymodule.py
+++ Tema 7. Unittest/mymodule.py
@@ -5,3 +5,26 @@ def sum(a, b):
         if not isinstance(n, int) and not isinstance(n, float):
             raise TypeError
     return a + b
+
+def get_prime_numbers(max_number):
+    numbers = [True, True] + [True] * (max_number-1)
+    last_prime_number = 2
+    i = last_prime_number
+    
+    while last_prime_number**2 <= max_number:
+        i += last_prime_number
+        while i <= max_number:
+            numbers[i] = False
+            i += last_prime_number
+        j = last_prime_number + 1
+        while j < max_number:
+            if numbers[j]:
+                last_prime_number = j
+                break
+            j += 1
+        i = last_prime_number
+    
+    return [i + 2 for i, not_crossed in enumerate(numbers[2:]) if not_crossed]
+
+def is_prime(n):
+    return n in get_prime_numbers(n)
--- Tema 7. Unittest/test_mymodule.py
+++ Tema 7. Unittest/test_mymodule.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 import unittest
 import mymodule
+
 class TestMyModule(unittest.TestCase):
+    
+    def test_get_prime_numbers(self):
+        self.assertEqual(mymodule.get_prime_numbers(10), [2, 3, 5, 7])
+    
+    def test_is_prime(self):
+        self.assertTrue(mymodule.is_prime(5))
+        self.assertFalse(mymodule.is_prime(6))
     
     def test_sum(self):
         self.assertEqual(mymodule.sum(5, 7), 12)
+        with self.assertRaises(TypeError):
+            mymodule.sum(5, "Python")
+
 if __name__ == "__main__":
     unittest.main()
